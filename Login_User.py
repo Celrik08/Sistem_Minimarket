6f9273356fa90c536013933899b6f41038c07b63
Celrik08 <m.fajar080907@gmail.com>
2025-11-22 12:59:12 +0800

    Menambahkan File nggk jelas

--- Login_User.py
+++
@@ -1,9 +0,0 @@
-# Fajar Login User
-from Data_User import muat_user
-
-def login_user(nama_user, kata_sandi):
-    user = muat_user()
-    for id_user, data in user.items():
-        if data.get("username") == nama_user and data.get("password") == kata_sandi:
-            return {"id": id_user, **data}
-    return None
